ac8359ed3cb122faaf59a153c7a0b75bc8a9ab51
Ruslan Iusupov <ruslanyussupov@gmail.com>
2018-03-27 16:43:33 +0300

    Done

---
+++ app/src/main/java/android/example/com/squawker/fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,20 @@
+package android.example.com.squawker.fcm;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String LOG_TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+    }
+
+
+}
